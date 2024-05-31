8c9e6eb54b8c6146746ef6877cf4705c48012ed7
coderithik@gmail.com <coderithik@gmail.com>
2024-05-31 21:48:45 +0530

    port fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 const cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: false }));
